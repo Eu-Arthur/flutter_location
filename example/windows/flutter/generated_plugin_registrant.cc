ddc3d96d4a2909272351b3fdfb684b4492d2d5f2
Arthur Eustache <arthut.eustache@gmail.com>
2023-06-07 11:45:41 +0200

    UPDATE Example SDK VERSION

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <location_windows/location_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  LocationWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("LocationWindows"));
 }
--- example/windows/flutter/generated_plugins.cmake
+++ example/windows/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  location_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
